a04b7534eeac5f01a98857e583a89e0d0492d3ce
kelle62819 <kelle62819@gmail.com>
2020-04-18 21:36:38 +0200

    trying request get load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,37 @@ export class AppComponent {
   audiosFB: Observable<any[]>;
   audio: any
   url: string
+  audioContext = new AudioContext()
+  currentBuffer  = null
   constructor(db: AngularFireDatabase) {
     this.audiosFB = db.list('audio').valueChanges();
     this.audiosFB.subscribe((audios)=>{
       console.log(audios)
       this.audio = audios[this.getRandomInt(audios.length)]
+      this.loadMusic(this.audio.url)
     })
   }
   getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
+
+loadMusic(url) {   
+    var req = new XMLHttpRequest();
+    req.open( "GET", url, true );
+    req.responseType = "arraybuffer";   
+    let th = this 
+    req.onreadystatechange = function (e) {
+          if (req.readyState == 4) {
+             if(req.status == 200)
+                  th.audioContext.decodeAudioData(req.response, 
+                    function(buffer) {
+                             this.currentBuffer = buffer;
+                             //displayBuffer(buffer);
+                    });
+             else
+                  alert('error during the load.Wrong url or cross origin issue');
+          }
+    } ;
+    req.send();
+}
 }
