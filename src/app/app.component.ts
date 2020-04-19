91e205addfb97307bdce55f95eec7d0ad71836ba
kelle62819 <kelle62819@gmail.com>
2020-04-19 09:10:28 +0200

    pudding images talking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 @Component({
@@ -11,14 +11,40 @@ export class AppComponent {
   audiosFB: Observable<any[]>;
   audio: any
   url: string
+  audioObj: any 
+  audioContext = new AudioContext()
+  imgIdx:number = 0
+  images: any[] = [
+    {src: "/assets/0.jpg"},
+    {src: "/assets/1.jpg"},
+    {src: "/assets/2.jpg"},
+    {src: "/assets/3.jpg"},
+    {src: "/assets/4.jpg"},
+    {src: "/assets/5.jpg"},
+  ]
+  @ViewChild('player', {static: false}) player:ElementRef
+  @ViewChild('anim', {static: false}) anim:ElementRef
   constructor(db: AngularFireDatabase) {
     this.audiosFB = db.list('audio').valueChanges();
     this.audiosFB.subscribe((audios)=>{
-      console.log(audios)
       this.audio = audios[this.getRandomInt(audios.length)]
+      this.audioContext.createMediaElementSource(this.player.nativeElement)
     })
   }
   getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
+  }
+  wisdom(){
+    this.player.nativeElement.play()
+    let index = 0;
+    setInterval(()=>{
+        if(index<this.audio.wave.length){
+          let calc = Math.round(this.audio.wave[index]*4)
+          if(calc > 4)
+           calc = 4
+          this.imgIdx = calc
+          index++
+        }
+    },100)
   }
 }
